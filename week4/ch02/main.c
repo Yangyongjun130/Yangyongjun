3da2c92c7bbad84e17d25c42625eeb33c2cf49f4
Yangyongjun130 <91767339+Yangyongjun130@users.noreply.github.com>
2021-10-24 20:25:06 +0800

    按大小实现union

---
+++ week4/ch02/main.c
@@ -0,0 +1,62 @@
+#ifndef DisjSet_H
+#define DisjSet_H
+#define NumSets 22
+
+typedef int DisjSet[NumSets + 1];
+typedef int SetType;
+typedef int ElementType;
+
+void Initilialize(DisjSet S);
+void SetUnion(DisjSet s, SetType root1, SetType root2);
+SetType Find(ElementType X, DisjSet S);
+#endif
+#define _CRT_SECURE_NO_WARNINGS
+#include "../ADT/head.h"
+#include <stdio.h>
+
+
+
+void initilialize(DisjSet s) {
+	for (int i = NumSets; i > 0; i--)
+		s[i] = 0;
+}
+//按大小
+void setUnion2(DisjSet s, SetType root1, SetType root2) {
+	int tmp = s[root1] + s[root2];
+	if (s[root2] < s[root1]) {
+		s[root1] = root2;
+		s[root2] = tmp;
+	}
+	else {
+		if (root2 == root1)
+			s[root2] = root1;
+	}
+}
+
+SetType find(ElementType X, DisjSet s) {
+	if (s[X] <= 0)
+		return X;
+	else
+		return s[X] = find(s[X], s);
+}
+int main() {
+	DisjSet s;
+	initilialize(s);
+	int u, v;
+	printf("enter the connection\n");
+	while (scanf("%d%d", &u, &v) == 2)
+	{
+		SetType utype = find(u, s);
+		SetType ttype = find(v, s);
+		if (utype == ttype)
+		{
+			printf("has connected\n");
+		}
+		else
+		{
+			printf("conncet them\n");
+			setUnion(s, utype, ttype);
+		}
+		printf("enter the connection\n");
+	}
+}
